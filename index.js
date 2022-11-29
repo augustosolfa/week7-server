ee289f2ba62a96639df5ef2317cf2c090e4f6480
Augusto Solfa <augusto@solfa.net>
2022-11-29 08:04:59 -0300

    Iteração 2

--- index.js
+++ index.js
@@ -47,6 +47,8 @@ const { PORT } = process.env;
 const app = express();
 app.use(express.json());
 
+// Iteração 1
+
 app.get("/all", (req, res) => res.status(200).json(bd));
 
 app.post("/create", (req, res) => {
@@ -76,6 +78,47 @@ app.delete("/delete/:id", (req, res) => {
   const index = bd.indexOf(processo);
   bd.splice(index, 1);
   return res.status(202).json();
+});
+
+// Iteração 2
+
+app.get("/process/:id", (req, res) => {
+  const { id } = req.params;
+  const processo = getById(id);
+  if (!processo) {
+    return res.status(404).json(`O id ${id} não consta na base de dados.`);
+  }
+  return res.status(200).json(processo);
+});
+
+app.put("/addComment/:id", (req, res) => {
+  const { id } = req.params;
+  const processo = getById(id);
+  if (!processo) {
+    return res.status(404).json(`O id ${id} não consta na base de dados.`);
+  }
+  const comentario = req.body["comment"];
+  if (!comentario) {
+    return res
+      .status(400)
+      .json(
+        `Requisição sem comentários a acrescentar. Formato {"comment": "Texto do comentário"}`
+      );
+  }
+  processo.comments.push(comentario);
+  return res.status(202).json(processo);
+});
+
+app.get("/status/open", (req, res) => {
+  const open = bd.filter((processo) => processo.status === "Em andamento");
+  console.log(bd);
+  return res.status(200).json(open);
+});
+
+app.get("/status/close", (req, res) => {
+  const open = bd.filter((processo) => processo.status === "Finalizado");
+  console.log(bd);
+  return res.status(200).json(open);
 });
 
 app.listen(PORT, () => {
